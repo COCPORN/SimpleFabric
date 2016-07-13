39b010b54c701f83c16e8f9138ff09443ec35613
Lars Thomas Denstad <ltd@flare.zone>
2016-07-14 00:21:32 +0200

    This starts implementing the state manager

---
+++ src/SimpleFabric/Actors/Runtime/IActorStateManager.cs
@@ -0,0 +1,26 @@
+﻿using SimpleFabric.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleFabric.Actors.Runtime
+{
+    public interface IActorStateManager
+    {
+        Task<T> AddOrUpdateStateAsync<T>(string stateName, T addValue, Func<string, T, T> updateValueFactory, CancellationToken cancellationToken = default(CancellationToken));      
+        Task AddStateAsync<T>(string stateName, T value, CancellationToken cancellationToken = default(CancellationToken));    
+        Task ClearCacheAsync(CancellationToken cancellationToken = default(CancellationToken));       
+        Task<bool> ContainsStateAsync(string stateName, CancellationToken cancellationToken = default(CancellationToken));      
+        Task<T> GetOrAddStateAsync<T>(string stateName, T value, CancellationToken cancellationToken = default(CancellationToken));    
+        Task<T> GetStateAsync<T>(string stateName, CancellationToken cancellationToken = default(CancellationToken));      
+        Task<IEnumerable<string>> GetStateNamesAsync(CancellationToken cancellationToken = default(CancellationToken));     
+        Task RemoveStateAsync(string stateName, CancellationToken cancellationToken = default(CancellationToken));    
+        Task SetStateAsync<T>(string stateName, T value, CancellationToken cancellationToken = default(CancellationToken));    
+        Task<bool> TryAddStateAsync<T>(string stateName, T value, CancellationToken cancellationToken = default(CancellationToken));      
+        Task<ConditionalValue<T>> TryGetStateAsync<T>(string stateName, CancellationToken cancellationToken = default(CancellationToken));   
+        Task<bool> TryRemoveStateAsync(string stateName, CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
---
+++ src/SimpleFabric/Data/ConditionalValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFabric.Data
+{
+    public struct ConditionalValue<TValue>
+    {
+        bool hasValue;
+        TValue value;
+
+        public ConditionalValue(bool hasValue, TValue value)
+        {
+            this.hasValue = hasValue;
+            this.value = value;
+        }
+
+        public bool HasValue { get { return hasValue; } }
+        public TValue Value {  get { return value; } }
+    }
+}
