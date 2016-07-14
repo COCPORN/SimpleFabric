3afb34828c767ce7ef68b32cff844bfc027b166d
Lars Thomas Denstad <ltd@flare.zone>
2016-07-14 18:11:57 +0200

    Initial commit of AzureTableStorage-based StateManager (not finished)

---
+++ src/SimpleFabric/Actors.StateManager.AzureTableStorage/AzureTableStorageActorStateManager.cs
@@ -0,0 +1,36 @@
+﻿using SimpleFabric.Actors.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using SimpleFabric.Actors.Implementation;
+using System.Configuration;
+
+namespace SimpleFabric.Actors.StateManager.AzureTableStorage
+{
+    public class AzureTableStorageActorStateManager : InMemoryActorStateManager, IActorAwareStateManager
+    {
+        string tableStorageConnection;
+        string tableStorageTable;
+
+        public Actor Actor { get; set; }
+
+        public AzureTableStorageActorStateManager()
+        {
+            GetConfiguration();
+        }
+
+        private void GetConfiguration()
+        {
+            tableStorageConnection = ConfigurationManager.AppSettings["AzureTableStorage.Connection"];
+            tableStorageTable = ConfigurationManager.AppSettings["AzureTableStorage.Table"];
+        }
+
+        public void Initialize()
+        {
+            tableStorageTable.Replace("{{actorTypeName}}", Actor.GetType().Name);
+        }
+    }
+}
---
+++ src/SimpleFabric/Actors.StateManager.AzureTableStorage/PartitionKeyCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SimpleFabric.Actors.Runtime;
+
+namespace Actors.StateManager.AzureTableStorage
+{
+    class PartitionKeyCreator
+    {
+        public static string CalculatePartitionKey(Actor actor)
+        {        
+            if (actor.Id == null) throw new InvalidOperationException("Actor is missing ActorId");
+
+            var r_partitionKey = 
+                actor.GetType().Name + ":" 
+                + actor.Id.ActorIdKind.ToString() + ":";
+
+            switch (actor.Id.ActorIdKind)
+            {
+                case SimpleFabric.Actors.ActorIdKind.Guid:
+                    r_partitionKey += actor.Id.GetGuidId().ToString();
+                    break;
+                case SimpleFabric.Actors.ActorIdKind.Long:
+                    r_partitionKey += actor.Id.GetLongId().ToString();
+                    break;
+                case SimpleFabric.Actors.ActorIdKind.String:
+                    r_partitionKey += actor.Id.GetStringId().ToString();
+                    break;
+            }
+            return r_partitionKey;
+        }
+    }
+}
