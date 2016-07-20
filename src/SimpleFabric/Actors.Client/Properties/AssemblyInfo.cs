5dda710425f8ffb77fc7ca0601f303a02aac5b44
Lars Thomas Denstad <ltd@flare.zone>
2016-07-20 20:14:51 +0200

    Started adding what is needed for nuget packing

--- src/SimpleFabric/Actors.Client/Properties/AssemblyInfo.cs
+++ src/SimpleFabric/Actors.Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Proxy")]
+[assembly: AssemblyTitle("SimpleFabric")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Proxy")]
+[assembly: AssemblyProduct("SimpleFabric")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
